d0c61fca96d4ada1f479f74dad8c084d9574c06b
hwaterke <hwaterke@users.noreply.github.com>
2026-05-15 21:25:32 +0200

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  endOfLine: 'lf',
+  bracketSpacing: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
+
+export default config
